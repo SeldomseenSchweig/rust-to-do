4d413e0e3131648f1619357914d035e6a5a30be6
SeldomseenSchweig <jamesschweig@gmail.com>
2024-07-18 11:13:05 -0500

    added todo list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::HashMap;
+
+
 fn main() {
-    println!("Hello, world!");
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
+
+
+
+}
+
+struct Todo {
+    // use rust built in HashMap to store key - val pairs
+    map: HashMap<String, bool>,
+}
+impl Todo {
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map.
+        // we pass true as value
+        self.map.insert(key, true);
+    }
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
 }
